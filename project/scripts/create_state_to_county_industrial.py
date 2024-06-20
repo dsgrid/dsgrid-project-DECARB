64af2e41f62e9aad13a2ab370eeacfe44091a3b8
lixiliu <36629962+lixiliu@users.noreply.github.com>
2024-06-20 13:45:08 -0600

    fix mapping

--- project/scripts/create_state_to_county_industrial.py
+++ project/scripts/create_state_to_county_industrial.py
@@ -14,9 +14,10 @@ def main():
     df = df.rename(columns=col_rename)[col_rename.values()]
 
     df["from_fraction"] = 1
-    df["from_fraction"] = df.set_index("from_id")["from_fraction"].div(df.groupby("from_id")["from_fraction"].sum()).round(9).values
+    df["from_fraction"] = df.groupby("from_id")["from_fraction"].transform(lambda x: x/x.sum()).round(8)
 
     # Check
+    df.groupby("from_id")["from_fraction"].sum().round(5).unique().tolist() == [1], "from fraction does not equal to 1 when grouped by from_id"
     df_state = pd.read_csv(state_file)
     diff = set(df["from_id"].unique()) - set(df_state["id"].to_list())
     assert len(diff) == 0, f"from_id has states not in dimension record: \n{diff}"
